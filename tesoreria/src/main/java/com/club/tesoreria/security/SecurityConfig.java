77697ff668f0949d46173b95710a948be99b1c24
Raul Ip <rauljouman@gmail.com>
2026-06-02 19:57:58 +0200

    CORS configuracion

--- tesoreria/src/main/java/com/club/tesoreria/security/SecurityConfig.java
+++ tesoreria/src/main/java/com/club/tesoreria/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
